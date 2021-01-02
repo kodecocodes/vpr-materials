d8889f1461cf0c8c390397f563e6981fd50680f3
Tim <0xtimc@gmail.com>
2021-01-02 21:54:58 +0000

    Update final proejct

--- 20-cookies-sessions/projects/final/TILApp/Sources/App/Migrations/CreateToken.swift
+++ 20-cookies-sessions/projects/final/TILApp/Sources/App/Migrations/CreateToken.swift
@@ -27,81 +27,17 @@
 /// THE SOFTWARE.
 
 import Fluent
-import Vapor
 
-final class User: Model, Content {
-  static let schema = "users"
-  
-  @ID
-  var id: UUID?
-  
-  @Field(key: "name")
-  var name: String
-  
-  @Field(key: "username")
-  var username: String
-
-  @Field(key: "password")
-  var password: String
-  
-  @Children(for: \.$user)
-  var acronyms: [Acronym]
-  
-  init() {}
-  
-  init(id: UUID? = nil, name: String, username: String, password: String) {
-    self.name = name
-    self.username = username
-    self.password = password
+struct CreateToken: Migration {
+  func prepare(on database: Database) -> EventLoopFuture<Void> {
+    database.schema("tokens")
+      .id()
+      .field("value", .string, .required)
+      .field("userID", .uuid, .required, .references("users", "id", onDelete: .cascade))
+      .create()
   }
 
-  final class Public: Content {
-    var id: UUID?
-    var name: String
-    var username: String
-
-    init(id: UUID?, name: String, username: String) {
-      self.id = id
-      self.name = name
-      self.username = username
-    }
+  func revert(on database: Database) -> EventLoopFuture<Void> {
+    database.schema("tokens").delete()
   }
 }
-
-extension User {
-  func convertToPublic() -> User.Public {
-    return User.Public(id: id, name: name, username: username)
-  }
-}
-
-extension EventLoopFuture where Value: User {
-  func convertToPublic() -> EventLoopFuture<User.Public> {
-    return self.map { user in
-      return user.convertToPublic()
-    }
-  }
-}
-
-extension Collection where Element: User {
-  func convertToPublic() -> [User.Public] {
-    return self.map { $0.convertToPublic() }
-  }
-}
-
-extension EventLoopFuture where Value == Array<User> {
-  func convertToPublic() -> EventLoopFuture<[User.Public]> {
-    return self.map { $0.convertToPublic() }
-  }
-}
-
-extension User: ModelAuthenticatable {
-  static let usernameKey = \User.$username
-  static let passwordHashKey = \User.$password
-
-  func verify(password: String) throws -> Bool {
-    try Bcrypt.verify(password, created: self.password)
-  }
-}
-
-extension User: ModelSessionAuthenticatable {}
-extension User: ModelCredentialsAuthenticatable {}
--- 20-cookies-sessions/projects/final/TILApp/Sources/App/Migrations/CreateUser.swift
+++ 20-cookies-sessions/projects/final/TILApp/Sources/App/Migrations/CreateUser.swift
@@ -27,81 +27,19 @@
 /// THE SOFTWARE.
 
 import Fluent
-import Vapor
 
-final class User: Model, Content {
-  static let schema = "users"
-  
-  @ID
-  var id: UUID?
-  
-  @Field(key: "name")
-  var name: String
-  
-  @Field(key: "username")
-  var username: String
-
-  @Field(key: "password")
-  var password: String
-  
-  @Children(for: \.$user)
-  var acronyms: [Acronym]
-  
-  init() {}
-  
-  init(id: UUID? = nil, name: String, username: String, password: String) {
-    self.name = name
-    self.username = username
-    self.password = password
+struct CreateUser: Migration {
+  func prepare(on database: Database) -> EventLoopFuture<Void> {
+    database.schema("users")
+      .id()
+      .field("name", .string, .required)
+      .field("username", .string, .required)
+      .field("password", .string, .required)
+      .unique(on: "username")
+      .create()
   }
-
-  final class Public: Content {
-    var id: UUID?
-    var name: String
-    var username: String
-
-    init(id: UUID?, name: String, username: String) {
-      self.id = id
-      self.name = name
-      self.username = username
-    }
+  
+  func revert(on database: Database) -> EventLoopFuture<Void> {
+    database.schema("users").delete()
   }
 }
-
-extension User {
-  func convertToPublic() -> User.Public {
-    return User.Public(id: id, name: name, username: username)
-  }
-}
-
-extension EventLoopFuture where Value: User {
-  func convertToPublic() -> EventLoopFuture<User.Public> {
-    return self.map { user in
-      return user.convertToPublic()
-    }
-  }
-}
-
-extension Collection where Element: User {
-  func convertToPublic() -> [User.Public] {
-    return self.map { $0.convertToPublic() }
-  }
-}
-
-extension EventLoopFuture where Value == Array<User> {
-  func convertToPublic() -> EventLoopFuture<[User.Public]> {
-    return self.map { $0.convertToPublic() }
-  }
-}
-
-extension User: ModelAuthenticatable {
-  static let usernameKey = \User.$username
-  static let passwordHashKey = \User.$password
-
-  func verify(password: String) throws -> Bool {
-    try Bcrypt.verify(password, created: self.password)
-  }
-}
-
-extension User: ModelSessionAuthenticatable {}
-extension User: ModelCredentialsAuthenticatable {}
