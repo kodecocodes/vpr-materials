3de14d1edb27a65b02e968129b492c9d94383b1d
Tim <0xtimc@gmail.com>
2021-01-12 16:41:30 +0000

    Add TLS config to MySQL setup

--- 36-microservices-redis/projects/starter/TILAppAcronyms/Sources/App/configure.swift
+++ 36-microservices-redis/projects/starter/TILAppAcronyms/Sources/App/configure.swift
@@ -44,7 +44,8 @@ public func configure(_ app: Application) throws {
     port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
     username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
     password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-    database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+    tlsConfiguration: .forClient(certificateVerification: .none)
   ), as: .mysql)
 
   app.migrations.add(CreateAcronym())
