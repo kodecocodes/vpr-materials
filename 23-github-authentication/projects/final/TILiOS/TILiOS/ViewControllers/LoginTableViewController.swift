591874c759256a9e367f3eec63dd0c7f7476336a
Richard Critz <rcritz@gmail.com>
2021-02-08 17:03:30 -0500

    Style Guide compliance

--- 23-github-authentication/projects/final/TILiOS/TILiOS/ViewControllers/LoginTableViewController.swift
+++ 23-github-authentication/projects/final/TILiOS/TILiOS/ViewControllers/LoginTableViewController.swift
@@ -72,7 +72,13 @@ class LoginTableViewController: UITableViewController {
     }
     let scheme = "tilapp"
     let session = ASWebAuthenticationSession(url: googleAuthURL, callbackURLScheme: scheme) { callbackURL, error in
-      guard error == nil, let callbackURL = callbackURL else { return }
+      guard 
+        error == nil, 
+        let callbackURL = callbackURL 
+      else { 
+        return 
+      }
+
       let queryItems = URLComponents(string: callbackURL.absoluteString)?.queryItems
       let token = queryItems?.first { $0.name == "token" }?.value
       Auth().token = token
@@ -92,7 +98,13 @@ class LoginTableViewController: UITableViewController {
     }
     let scheme = "tilapp"
     let session = ASWebAuthenticationSession(url: githubAuthURL, callbackURLScheme: scheme) { callbackURL, error in
-      guard error == nil, let callbackURL = callbackURL else { return }
+      guard
+        error == nil,
+        let callbackURL = callbackURL
+      else {
+        return
+      }
+
       let queryItems = URLComponents(string: callbackURL.absoluteString)?.queryItems
       let token = queryItems?.first { $0.name == "token" }?.value
       Auth().token = token
